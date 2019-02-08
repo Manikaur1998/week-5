509d67742b250d62168cbd46eb98bf0d42b9f10f
visualstudio <visualstudio@122-PC29>
2019-02-08 17:56:24 -0500

    starting

---
+++ wEEK5_1/wEEK5_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wEEK5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int   i = 7;
+            int j = 5;
+            int m = 8;
+            Console.ReadLine();
+        }
+    }
+}
